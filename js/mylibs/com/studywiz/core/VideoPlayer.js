ca53dd090425f6442ed556619d2be152f3ef64da
Radek Petr <radek@netsafe.cz>
2012-08-07 00:13:04 +0200

    Using Moo

--- js/mylibs/com/studywiz/core/VideoPlayer.js
+++ js/mylibs/com/studywiz/core/VideoPlayer.js
@@ -1,38 +1,38 @@
-function VideoPlayer() {
-   alert ("Alive");
-}
+var VideoPlayer = new Class({
+    initialize : function() {
+        // alert("Alive");
+    },
+    setSource : function(src) {
+        this.filename = src;
+        this.myVideoPlayer.src([{
+            type : "video/mp4",
+            src : "http://www.jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v"
+        }, {
+            type : "video/webm",
+            src : "http://video-js.zencoder.com/oceans-clip.webm"
+        }, {
+            type : "video/ogg",
+            src : "http://video-js.zencoder.com/oceans-clip.ogv"
+        }]);
+    },
+    play : function() {
+        this.myVideoPlayer.play();
+    },
+    addVideoPlayer : function(divName) {
+        videoElement = document.createElement("video");
+        videoElement.setAttribute("id", divName);
 
-VideoPlayer.prototype.setSource = function(src) {
-    this.filename = src;
-    this.myVideoPlayer.src([{
-        type : "video/mp4",
-        src : "http://www.jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v"
-    }, {
-        type : "video/webm",
-        src : "http://video-js.zencoder.com/oceans-clip.webm"
-    }, {
-        type : "video/ogg",
-        src : "http://video-js.zencoder.com/oceans-clip.ogv"
-    }]);
-}
+        videoElement.setAttribute("preload", "auto");
+        videoElement.setAttribute("width", "800");
+        videoElement.setAttribute("height", "600");
+        videoElement.setAttribute("poster", "http://video-js.zencoder.com/oceans-clip.png");
 
-VideoPlayer.prototype.play = function() {
-    this.myVideoPlayer.play();
-}
+        videoDiv = document.createElement('div');
+        document.body.appendChild(videoDiv);
+        videoDiv.appendChild(videoElement);
 
-VideoPlayer.prototype.addVideoPlayer = function (divName) {
-    videoElement = document.createElement("video");
-    videoElement.setAttribute("id", divName);
+        this.myDiv = divName;
+        this.myVideoPlayer = _V_(this.myDiv);
+    }
+})
 
-    videoElement.setAttribute("preload", "auto");
-    videoElement.setAttribute("width", "800");
-    videoElement.setAttribute("height", "600");
-    videoElement.setAttribute("poster", "http://video-js.zencoder.com/oceans-clip.png");
-
-    videoDiv = document.createElement('div');
-    document.body.appendChild(videoDiv);
-    videoDiv.appendChild(videoElement);
-    
-    this.myDiv = divName;
-    this.myVideoPlayer = _V_(this.myDiv);
-}
