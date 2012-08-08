d4b8bc4a31cae0e61cb2ddfb14cf5a2789829225
Radek Petr <radek@netsafe.cz>
2012-08-08 21:21:07 +0200

    Implemented preloading of JS libraries

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/**
+ * @author Radek
+ */
+
+var Main = new Class({
+    Implements : Events,
+    initialize : function() {
+        // load external js libraries so they are available to the project
+        var _self = this;
+        this.listOfLibraries = new Array("js/mylibs/createjs/soundjs-0.2.0.min.js", "js/mylibs/video-js/video.js", "js/mylibs/com/studywiz/core/VideoPlayer.js")
+        this.listOfLibrariesCounter = 0;
+        this.listOfLibraries.each(function(item, index) {
+            this._loadLibrary(item, index)
+        }.bind(this))
+    },
+
+    start : function() {
+        var vp = new VideoPlayer();
+        vp.addVideoPlayer("myPlayer");
+        vp.setSource("bbb");
+        vp.play();
+    },
+    
+    // PRIVATE - load external js libraries so they are available to the project
+    _loadLibrary : function(item, index) {
+        
+        var loadJS = new Asset.javascript(item, {
+            id : index,
+            onLoad : function() {
+               // alert(item);
+                this.listOfLibrariesCounter++;
+                if (this.listOfLibrariesCounter == this.listOfLibraries.length) {
+                    this.fireEvent('READY');
+                }
+            }.bind(this)
+        });
+    }.protect()
+})
+
