92f840b906ff0f7dbe7971ce385bc138cd29a8f7
Radek Petr <radek@netsafe.cz>
2012-08-10 13:07:42 +0200

    Refactoring, added show, hide

--- js/main.js
+++ js/main.js
@@ -8,16 +8,7 @@ var Main = new Class({
     initialize : function() {
         // load external js libraries so they are available to the project
         var _self = this;
-        this.listOfLibraries = new Array(
-            "js/mylibs/createjs/tweenjs-0.2.0.min.js",
-            "js/mylibs/createjs/easeljs-0.4.2.min.js",
-            "js/libs/jquery-1.6.2.min.js",
-            "js/mylibs/createjs/soundjs-0.2.0.min.js",
-            "js/mylibs/video-js/video.js",
-            "js/mylibs/com/studywiz/core/VideoPlayer.js",
-            "js/mylibs/com/studywiz/core/AudioPlayer.js",
-            "js/mylibs/createjs/preloadjs-0.1.0.min.js"
-            )
+        this.listOfLibraries = new Array("js/mylibs/createjs/easeljs-0.4.2.min.js", "js/libs/jquery-1.6.2.min.js", "js/mylibs/createjs/soundjs-0.2.0.min.js", "js/mylibs/video-js/video.js", "js/mylibs/com/studywiz/core/VideoPlayer.js", "js/mylibs/com/studywiz/core/AudioPlayer.js", "js/mylibs/createjs/preloadjs-0.1.0.min.js")
         this.listOfLibrariesCounter = 0;
         this.listOfLibraries.each( function(item, index) {
             this._loadLibrary(item, index)
@@ -25,20 +16,38 @@ var Main = new Class({
     },
     // ----------------------------------------------------------
     start : function() {
-        var vp = new VideoPlayer();
-        vp.addVideoPlayer("myPlayer");
-        vp.setSource("bbb");
-        vp.play();        
+        var vp = new VideoPlayer("video_1");
+        var params = new Object();
+        params.source = [{
+            type : "video/mp4",
+            src : "media/video/country/country_cla01_next.m4v"
+        }, {
+            type : "video/webm",
+            src : "media/video/country/country_cla01_next.webm"
+        }, {
+            type : "video/ogg",
+            src : "media/video/country/country_cla01_next.ogg"
+        }];
+        params.poster = {
+            src : "http://video-js.zencoder.com/oceans-clip.png"
+        };
+        vp.setParams(params);
+        vp.addVideoPlayer();
+        vp.show();
+        
+        vp.play();
+        
         var sound = new AudioPlayer();
-        sound.setSource ("media/sound/country/mp3/country_accident.mp3|media/sound/country/mp3/country_accident.ogg","Sound_1")
+        sound.setSource("media/sound/country/mp3/country_accident.mp3|media/sound/country/mp3/country_accident.ogg", "Sound_1")
         sound.play();
+        vp.hide();
     },
     // ----------------------------------------------------------
     // PRIVATE - load external js libraries so they are available to the project
     _loadLibrary : function(item, index) {
         var newAsset = new Asset.javascript(item, {
             id : index,
-            onLoad : function() {                
+            onLoad : function() {
                 this.listOfLibrariesCounter++;
                 if (this.listOfLibrariesCounter == this.listOfLibraries.length) {
                     this.fireEvent('READY');
--- js/mylibs/com/studywiz/core/VideoPlayer.js
+++ js/mylibs/com/studywiz/core/VideoPlayer.js
@@ -1,44 +1,47 @@
 var VideoPlayer = new Class({
 
-    initialize : function() {
-        // alert("Alive");
+    initialize : function(myID) {
+        this.id = myID
+        this.videoSource = new Array();
+        this.myVideoPlayer = {};
+        this.videoElement = new Element("video", {
+            id : this.id,
+            preload : "auto",
+            width : "800",
+            height : "600",
+            poster : ""
+        });
+
+        console.log("-------------- Created Video Player: " + myID);
     },
     // ---------------------------
-    setSource : function(src) {
-        this.filename = src;
-        this.myVideoPlayer.src([{
-            type : "video/mp4",
-            src : "media/video/country/country_cla01_next.m4v"
-        }, {
-            type : "video/webm",
-            src : "media/video/country/country_cla01_next.webm"
-        }, {
-            type : "video/ogg",
-            src : "media/video/country/country_cla01_next.ogg"
-        }]);
+    setParams : function(params) {
+        this.videoSource = params.source;
+        this.videoElement.setProperty("poster", params.poster.src)
     },
     // ---------------------------
     play : function() {
+        this.myVideoPlayer = _V_(this.id);
+        this.myVideoPlayer.src(this.videoSource);
         this.myVideoPlayer.ready((function(videoPlayer) {
             this.myVideoPlayer.play();
         }).bind(this))
     },
     // ---------------------------
-    addVideoPlayer : function(divName) {
-        videoElement = document.createElement("video");
-        videoElement.setAttribute("id", divName);
-
-        videoElement.setAttribute("preload", "auto");
-        videoElement.setAttribute("width", "800");
-        videoElement.setAttribute("height", "600");
-        videoElement.setAttribute("poster", "http://video-js.zencoder.com/oceans-clip.png");
-
-        videoDiv = document.createElement('div');
-        document.body.appendChild(videoDiv);
-        videoDiv.appendChild(videoElement);
-
-        this.myDiv = divName;
-        this.myVideoPlayer = _V_(this.myDiv);
+    addVideoPlayer : function() {
+        var videoDiv = new Element("div", {
+            id : "videoHolder"
+        });
+        videoDiv.inject(document.body);
+        this.videoElement.inject(videoDiv)
+    },
+    
+    show : function (){
+        this.videoElement.show();
+    }, 
+    
+    hide : function () {
+        this.videoElement.hide();
     }
 })
 
