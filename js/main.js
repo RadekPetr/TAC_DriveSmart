a1642c7c4cb669390c11a975abb14a68e57f0ad0
Radek Petr <radek@netsafe.cz>
2012-08-28 17:14:18 +0200

    Added automatic loading of esxternal assets by their extension (js and css supported Loading video.js css

--- js/main.js
+++ js/main.js
@@ -6,18 +6,20 @@ var Main = new Class({
     Implements : Events,
     // ----------------------------------------------------------
     initialize : function() {
+
         // load external js libraries so they are available to the project
         var _self = this;
-        this.listOfLibraries = new Array("js/mylibs/createjs/easeljs-0.4.2.min.js", "js/libs/jquery-1.6.2.min.js", "js/mylibs/createjs/soundjs-0.2.0.min.js", "js/mylibs/video-js/video.js", "js/mylibs/com/studywiz/core/VideoPlayer.js", "js/mylibs/com/studywiz/core/AudioPlayer.js", "js/mylibs/com/studywiz/core/Button.js" , "js/mylibs/createjs/preloadjs-0.1.0.min.js", "js/mylibs/com/studywiz/units/Unit.js" )
+        this.listOfLibraries = new Array("js/mylibs/video-js/video-js.css", "js/mylibs/createjs/easeljs-0.4.2.min.js", "js/libs/jquery-1.6.2.min.js", "js/mylibs/createjs/soundjs-0.2.0.min.js", "js/mylibs/video-js/video.js", "js/mylibs/com/studywiz/core/VideoPlayer.js", "js/mylibs/com/studywiz/core/AudioPlayer.js", "js/mylibs/com/studywiz/core/Button.js", "js/mylibs/createjs/preloadjs-0.1.0.min.js", "js/mylibs/com/studywiz/units/Unit.js")
         this.listOfLibrariesCounter = 0;
         this.listOfLibraries.each( function(item, index) {
-            this._loadLibrary(item, index)
+            this._loadAsset(item, index)
         }.bind(this))
+
     },
     // ----------------------------------------------------------
     start : function() {
         var myUnit = new Unit();
-       
+
         myUnit.start();
         // this.vp.start();
         // this.vp.addEvent("TIMELINE", this._handleTimelineEvents);
@@ -39,18 +41,37 @@ var Main = new Class({
     },
     // ----------------------------------------------------------
     // PRIVATE - load external js libraries so they are available to the project
-    _loadLibrary : function(item, index) {
-        var newAsset = new Asset.javascript(item, {
-            id : index,
-            onLoad : function() {
-                this.listOfLibrariesCounter++;
-                if (this.listOfLibrariesCounter == this.listOfLibraries.length) {
-                    this.fireEvent('READY');
-                    console.log("Fired event READY");
-                }
-            }.bind(this)
-        });
+    _loadAsset : function(item, index) {
+        var fileType = item.split('.').pop();
+        switch (fileType) {
+            case "js" :
+                var newAsset = new Asset.javascript(item, {
+                    id : index,
+                    onLoad : function() {
+
+                        this._loadedFinished();
+                    }.bind(this)
+                });
+                break;
+            case  "css" :
+                var newAsset = new Asset.css(item, {
+                    id : index,
+                    onLoad : function() {
+
+                        this._loadedFinished();
+                    }.bind(this)
+                });
+                break;
+        }
     }.protect(),
+
+    _loadedFinished : function() {
+        this.listOfLibrariesCounter++;
+        if (this.listOfLibrariesCounter == this.listOfLibraries.length) {
+            this.fireEvent('READY');
+            console.log("Fired event READY");
+        }
+    },
     // ----------------------------------------------------------
     // PRIVATE - handle events for timeline
     _handleTimelineEvents : function(params) {
