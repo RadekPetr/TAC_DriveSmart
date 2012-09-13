34f6a17dbeb1115ac1e4462ccf17c5850a1d78da
Radek Petr <radek@netsafe.cz>
2012-09-13 17:09:35 +0200

    Utils created - calculate position

---
+++ js/mylibs/com/studywiz/core/Utils.js
@@ -0,0 +1,10 @@
+/**
+ * @author Radek
+ */
+function getPos(el) {
+    // yay readability
+    for (var lx=0, ly=0;
+         el != null;
+         lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
+    return {x: lx,y: ly};
+}
