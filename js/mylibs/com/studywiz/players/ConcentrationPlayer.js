a65f422d4b784a6f7636ea7ec53c73042c1c28ef
Radek Petr <petr.radek@gmail.com>
2012-12-20 00:23:35 +0100

    deleted useless stuff

--- js/mylibs/com/studywiz/players/ConcentrationPlayer.js
+++ js/mylibs/com/studywiz/players/ConcentrationPlayer.js
@@ -1,12 +1,11 @@
 /*
 
-function ExerciseComplete(iResult){
-									
+function ExerciseComplete(iResult){									
 	
 	if(iResult.toString() == "NaN"){
 		iResult=Number(0)
 	}
-	//iResult = Number(iResult)
+	
 	drace("Exercise Complete "+iResult)
 	Background.gotoAndPlay("concentration2")
 	movie_holder.unloadMovie()								//Close movie
@@ -33,10 +32,9 @@ function GoNextCon(){
 			StartButton._visible=1
 			Background.gotoAndPlay("concentration2")
 			RunExercise()		//We are not returning to Concentration menu here but going straight to the next exercise
-
-		}
-	
+		}	
 }
+
 function CalcLevel(){
 	if(GetAttribute(ContNode,"calc")!="true") {	//Continue onto Ad if no calc needed
 			DoAd()
@@ -50,21 +48,8 @@ function CalcLevel(){
 	}
 	
 	drace("Processing calc")
-	//NOT USED     //FIND THE BASELINE
-	//NOT USED     gBaseLineScore=0;
+	
 	totalScore=0					//Total score for the current section, not really used for ex8
-	if(eTracker.seq==14){				
-		//Ex 14 needs to see the baseline for only two exercises 
-		//NOT USED     gBaseLineScore=( eTracker.score[2] + eTracker.score[5])/2	//We score exercises 3 and 6 and get the average
-		//NOT USED     drace("BASELINE CALC for EX 14"+gBaseLineScore)
-	//NOT USED     }else{
-		//get the average for the section prior to the one we just completed
-		//NOT USED     for(i=eTracker.score.length-12;i<eTracker.score.length-6;i++){			
-		//NOT USED     	//NOT USED     gBaseLineScore+=eTracker.score[i]					//Add it up
-		//NOT USED     	drace("EX:"+i+"  = "+eTracker.score[i]+" T: "+gBaseLineScore)
-		//NOT USED     }		
-		//NOT USED     gBaseLineScore = gBaseLineScore/6	//The average
-	//NOT USED     }
 	
 		//Now get the average for the previous 6 exercises
 		for(i=eTracker.score.length-6;i<eTracker.score.length;i++){			
@@ -73,21 +58,13 @@ function CalcLevel(){
 		}
 	
 	
-	drace("Total Score: "+totalScore/6+" : Baseline "+gBaseLineScore)
+	drace("Total Score: "+totalScore/6")
 	ScoreAverage = totalScore/6	//Get the average value
 	
 	ScorePercentage = Math.round(ScoreAverage)
-	drace("Score: "+ScorePercentage+" : Baseline "+gBaseLineScore)
+	drace("Score: "+ScorePercentage")
 	drace("Calc a level jump...")
 	
-	
-	
-	//NOT USED     iThreshold = 10
-	//NOT USED     iScoreDiff = (100 - gBaseLineScore)/iThreshold		//Threshold = Baseline+(10% of difference)
-	//NOT USED     drace("iScoreDiff "+iScoreDiff)
-	//NOT USED     drace("Expecting "+(gBaseLineScore + iScoreDiff)+" for a level increase")
-	
-	//if( ScoreAverage >= (gBaseLineScore + iScoreDiff)){ //Ash muted this as we now jump if >=85
 		
 	if( ScoreAverage >= 85){	
 		// LEVEL INCREASE				
