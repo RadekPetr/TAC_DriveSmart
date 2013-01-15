5be7366250feac55cdba49beafd13f5fe821cd06
Radek Petr <petr.radek@gmail.com>
2013-01-15 15:59:56 +0100

    Simplified start, added build version

--- js/mylibs/Main.js
+++ js/mylibs/Main.js
@@ -8,8 +8,9 @@ var Main = new Class({
     Implements : [Options, Events],
     // ----------------------------------------------------------
     initialize : function(isDev) {
-       
-        if (isDev) {
+        log("****** Version: " + Main.version + " Build: " + Main.build + " ******");
+
+        if (isDev == true) {
             // load external js libraries so they are available to the project
 
             this.listOfLibraries = ["css/various.css", "css/dragndrop.css", "css/radios.css", "css/button.css", "js/mylibs/video-js/video-js.css", "css/progressBar.css", "css/main_menu.css", "js/mylibs/createjs/soundjs-0.3.0.min.js", "js/mylibs/video-js/video.js", "js/mylibs/com/studywiz/utils/consolelog.js", "js/mylibs/com/studywiz/ui/uiHelpers.js", "js/mylibs/com/studywiz/utils/Utils.js", "js/mylibs/com/studywiz/utils/Array.sortOn.js", "js/mylibs/com/studywiz/loaders/MediaLoader.js", "js/mylibs/com/studywiz/loaders/DataLoader.js", "js/mylibs/com/studywiz/ui/dwProgressBar.js", "js/mylibs/com/studywiz/players/ImagePlayer.js", "js/mylibs/com/studywiz/players/VideoPlayer.js", "js/mylibs/com/studywiz/players/DragNDropPlayer.js", "js/mylibs/com/studywiz/players/AudioPlayer.js", "js/mylibs/com/studywiz/ui/Button.js", "js/mylibs/com/studywiz/ui/CommentaryFeedback.js", "js/mylibs/createjs/preloadjs-0.2.0.min.js", "js/mylibs/com/studywiz/players/SequencePlayer.js", "js/mylibs/com/studywiz/players/ModulePlayer.js", "js/mylibs/com/studywiz/players/KeyRisksPlayer.js", "js/mylibs/com/studywiz/players/SwiffPlayer.js", "js/mylibs/com/studywiz/core/Modules.js", "js/mylibs/com/studywiz/core/User.js", "js/mylibs/com/studywiz/ui/Questions.js", "js/mylibs/com/studywiz/ui/MenuItems.js", "js/mylibs/com/studywiz/ui/Shape.js", "js/mylibs/com/studywiz/ui/Recorder.js", "js/mylibs/xml2json/xml2json.js", "js/mylibs/rightclick/rightClick.js", "js/mylibs/com/studywiz/players/Draggable.js"];
@@ -18,11 +19,12 @@ var Main = new Class({
             Array.each(this.listOfLibraries, function(item, index) {
                 this._loadAsset(item, index);
             }.bind(this));
+        } else {
+            this.start();
         }
     },
     // ----------------------------------------------------------
     start : function() {
-        console.log("Main");
         Main.sequencePlayer = new SequencePlayer(this, {});
         this.modules = new Modules({});
         this.modules.start();
@@ -57,7 +59,6 @@ var Main = new Class({
             //console.log("Fired event READY");
         }
     },
-   
 });
 
 // Add static variables
@@ -71,9 +72,10 @@ Main.paths = {
 
 Main.divID = 'drivesmart';
 Main.version = '1.0';
+Main.build = '2013/01/14';
 
 Main.sequencePlayer = null;
-Main.userTracker=null;
+Main.userTracker = null;
 
 Main.userDataStoreURL = '/user_progress';
 Main.userDataProgressURL = '/user_progress';
