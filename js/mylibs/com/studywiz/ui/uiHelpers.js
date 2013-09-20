053b9ba5ae3661094ca1ae3f4ba86332b87e90ee
Radek Petr <petr.radek@gmail.com>
2013-09-20 12:37:29 +0200

    Refactoring Setup Button

--- js/mylibs/com/studywiz/ui/uiHelpers.js
+++ js/mylibs/com/studywiz/ui/uiHelpers.js
@@ -54,3 +54,129 @@ UIHelpers.moduleProgressSetup = function(moduleID) {
     });
     return progress;
 };
+
+UIHelpers.setupButton2 = function(buttonType, parent, next) {
+    // TODO: position by class - predefine positions in css and give the buttons ids according to the position
+    var buttonPosition = {
+        x : 480,
+        y : 415
+    };
+    var presets = new Hash({
+        "Main Menu" : {
+            id : "button_" + parent.buttons.length,
+            text : "Main Menu",
+            next : "MainMenu.clicked",
+            style : {
+                left : buttonPosition.x,
+                top : buttonPosition.y - 45
+            }
+        },
+        "Continue" : {
+            id : "button_" + parent.buttons.length,
+            text : "Continue",
+            next : "Continue.clicked",
+            style : {
+                left : buttonPosition.x,
+                top : buttonPosition.y
+            }
+        },
+        "Repeat" : {
+            id : "button_" + parent.buttons.length,
+            text : "Repeat",
+            next : "Repeat.clicked",
+            style : {
+                left : buttonPosition.x,
+                top : buttonPosition.y - 90
+            }
+        },
+        "Done" : {
+            id : "button_" + parent.buttons.length,
+            text : "Done",
+            next : "Risks.done",
+            style : {
+                left : buttonPosition.x,
+                top : buttonPosition.y
+            }
+        },
+        "Cancel" : {
+            id : "button_" + parent.buttons.length,
+            text : "Cancel",
+            next : "ConActivity.cancel.clicked",
+            style : {
+                left : buttonPosition.x,
+                top : buttonPosition.y
+            }
+        },
+        "Record" : {
+            id : "button_" + parent.buttons.length,
+            text : "Record",
+            next : "CommentaryIntro.done",
+            style : {
+                left : buttonPosition.x,
+                top : buttonPosition.y
+            }
+        },
+        "Expert commentary" : {
+            id : "button_" + parent.buttons.length,
+            text : "Expert commentary",
+            next : "CommentaryIntro.expert.clicked",
+            style : {
+                left : 20,
+                top : buttonPosition.y,
+                width : '195px'
+            }
+        },
+        "Expert commentary 2" : {
+            id : "button_" + parent.buttons.length,
+            text : "Expert commentary",
+            next : "Commentary.expert.clicked",
+            style : {
+                left : 20,
+                top : buttonPosition.y - 45,
+                width : '195px'
+            }
+        },
+        "Skip" : {
+            id : "button_" + parent.buttons.length,
+            text : "Skip",
+            next : "ConIntro.done.clicked",
+            style : {
+                left : buttonPosition.x,
+                top : buttonPosition.y
+            }
+        },
+        "Start" : {
+            id : "button_" + parent.buttons.length,
+            text : "Start",
+            next : "ConIntro.done.clicked",
+            style : {
+                left : buttonPosition.x,
+                top : buttonPosition.y
+            }
+        },
+        "Your commentary" : {
+            id : "button_" + parent.buttons.length,
+            text : "Your commentary",
+            next : "Commentary.replay.clicked",
+            style : {
+                left : 20,
+                top : buttonPosition.y,
+                width : '195px'
+            }
+
+        }
+
+    });
+
+    var options = presets.get(buttonType);
+    // TODO: Disabled symbol on buttons, to finalize with new style when available
+    options['class'] = 'button';
+    if (next) {
+        options['next'] = next;
+    }
+    //
+    var button = new Button(parent, options);
+    button.add(Main.DIV_ID);
+    button.show();
+    parent.buttons.push(button);
+};
