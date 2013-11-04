8b3eac290c3683557a276c0c0a399ba08d2d3618
Radek Petr <petr.radek@gmail.com>
2013-11-04 23:00:14 +0100

    Local

--- js/mylibs/Main.js
+++ js/mylibs/Main.js
@@ -102,7 +102,7 @@ Main.VERSION = '1.0.1';
 Main.BUILD = '2013/11/03';
 
 // When running on localhost (So I can use different paths when testing)
-Main.IS_LOCAL = true;
+Main.IS_LOCAL = false;
 
 // Show hide debug panel and ignore lock status
 Main.DEBUG = true;
