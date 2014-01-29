d049d2e7a98753542dd1b82b4fe41565a6a29137
Radek Petr <petr.radek@gmail.com>
2014-01-29 16:29:23 +0100

    Show captions if default is set

--- js/mylibs/video-js/video.js
+++ js/mylibs/video-js/video.js
@@ -6674,6 +6674,13 @@ vjs.TextTrackMenuItem = vjs.MenuItem.extend({
     vjs.MenuItem.call(this, player, options);
 
     this.player_.on(track.kind() + 'trackchange', vjs.bind(this, this.update));
+    /**
+* @author Michal Korotkiewicz
+* my modification:
+*/
+if(track.dflt()) {
+    this.player_.showTextTrack(this.track.id_, this.track.kind());
+}
   }
 });
 
