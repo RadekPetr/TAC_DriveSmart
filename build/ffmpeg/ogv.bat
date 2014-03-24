7457b54dfd900528eac51646740452c799c38436
Radek Petr <petr.radek@gmail.com>
2014-03-24 16:13:11 +0100

    video encoding

--- build/ffmpeg/ogv.bat
+++ build/ffmpeg/ogv.bat
@@ -1,1 +1,1 @@
-"ffmpeg.exe" -i %1 -b:v 500k -maxrate 500k -bufsize 1000k -vcodec libtheora -quality best -acodec libvorbis -ab 128k %~p1%~n1.ogv
+"ffmpeg.exe" -i %1 -b:v 1500k -maxrate 1500k -bufsize 3000k -vcodec libtheora -quality best -acodec libvorbis -ab 128k %~p1%~n1.ogv
--- build/ffmpeg/webm.bat
+++ build/ffmpeg/webm.bat
@@ -1,2 +1,4 @@
-"ffmpeg.exe" -i %1 -b:v 500k -maxrate 500k -bufsize 1000k -vcodec libvpx -quality best -acodec libvorbis -ab 128k -y -f webm -g 60 %~p1%~n1.webm
-"ffmpeg.exe" -i %1 -b:v 500k -maxrate 500k -bufsize 1000k -vcodec libtheora -quality best -acodec libvorbis -y -ab 128k %~p1%~n1.ogv
+"ffmpeg.exe" -i %1 -b:v 1000k -maxrate 1500k -bufsize 3000k -c:v libvpx -crf 5 -qmin 0 -qmax 50 -vf scale=640:-1 -acodec libvorbis -ab 48k -y -f webm -g 50 %~p1%~n1.webm
+"ffmpeg.exe" -i %1 -b:v 1000k -maxrate 1500k -bufsize 3000k -vcodec libtheora -quality best -vf scale=640:-1 -acodec libvorbis -y -ab 48k %~p1%~n1.ogv
+#"ffmpeg.exe" -i %1 -y -c:v libx264 -preset veryslow -tune film -vf scale=640:-1 -b:v 1000k -pass 1 -maxrate 1500k -bufsize 3000k -strict experimental -c:a aac -b:a 48k %~p1%~n1.mp4 
+#"ffmpeg.exe" -i %1 -y -c:v libx264 -preset veryslow -tune film -vf scale=640:-1 -b:v 1000k -pass 2 -maxrate 1500k -bufsize 3000k -strict experimental -c:a aac -b:a 48k %~p1%~n1.mp4 
